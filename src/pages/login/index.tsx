0ce73cf9533dbf19be29e347f4bf168bb57f481a
Hussain <iamhussainali2@gmail.com>
2023-05-12 20:00:46 +0530

    fixed some changes in register page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import logo from '../../../public/Assets/graduation-hat.png';
 import bg from '../../../public/Assets/reshot-illustration-login-security-page-JNGY2AVZEB.png';
 import Link from 'next/link';
 import {FcGoogle} from 'react-icons/fc';
 import { AuthContext } from '@/Context/AuthProvider';
 import { useRouter } from 'next/router';
+import Aos from 'aos';
 
 
 const Login = () => {
@@ -39,12 +40,16 @@ const Login = () => {
         .catch((error:any)=> console.error(error))
     }
 
+    useEffect(()=>{
+        Aos.init()
+    },[])
+    
     return (
         <div className='min-h-screen login-wrapper relative grid grid-cols-2 items-center'>
-            <div>
+            <div data-aos="zoom-in" data-aos-duration="2000">
                 <Image src={bg} alt="login-bg"/>
             </div>
-            <div className='flex justify-center items-center'>
+            <div data-aos='fade-left' data-aos-duration="2000" className='flex justify-center items-center'>
                 <div className='w-3/4'>
                     <div className="nav-logo flex gap-2 items-center mb-5">
                         <Image src={logo} alt="" className="w-10" />
@@ -54,11 +59,11 @@ const Login = () => {
                     <form onSubmit={handleSubmit} className="login-form flex flex-col gap-5">
                         <div className='flex flex-col gap-2'>
                             <label>Email</label>
-                            <input type="text" name="email" required className='outline-none border focus:border-secondary py-2 px-3 rounded' />
+                            <input type="email" name="email" required placeholder='Enter Email' className='outline-none border focus:border-secondary py-2 px-3 rounded' />
                         </div>
                         <div className='flex flex-col gap-2'>
                             <label>Password</label>
-                            <input type="password" name="password" required className='outline-none border focus:border-secondary py-2 px-3 rounded' />
+                            <input type="password" name="password" required placeholder='Enter Password' className='outline-none border focus:border-secondary py-2 px-3 rounded' />
                         </div>
 
                         <button className='bg-secondary rounded font-medium text-white p-2 mb-2'>Login</button>
@@ -66,7 +71,7 @@ const Login = () => {
                         <div className='divider h-[1px] mb-6 text-center bg-gray-400'>
                             <span className='bg-white relative -top-[0.7em] px-4'>OR</span>
                         </div>
-                        <button type='button' onClick={handleGoogleSignIn} className='border rounded py-2 -mt-3 text-center relative font-medium hover:bg-slate-50 transition-colors'><span className='absolute left-2 top-1/2 -translate-y-[50%] text-2xl'><FcGoogle/></span><span>Log In with Google</span></button>
+                        <button type='submit' onClick={handleGoogleSignIn} className='border rounded py-2 -mt-3 text-center relative font-medium hover:bg-secondary hover:text-white transition-colors'><span className='absolute left-2 top-1/2 -translate-y-[50%] text-2xl'><FcGoogle/></span><span>Log In with Google</span></button>
                         <button>Forgot Password?</button>
                         <p className='text-center'>
                             New user? <Link href="/register">Create an account</Link>
