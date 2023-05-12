b0dcf0834e5fbadb437ac3bce438a1a1fbe7c81b
Hussain <iamhussainali2@gmail.com>
2023-05-12 21:14:52 +0530

    fixed some error in looking courses

--- src/components/looking-course-section/LookingCourseSection.tsx
+++ src/components/looking-course-section/LookingCourseSection.tsx
@@ -14,7 +14,7 @@ const LookingCourseSection = () => {
         <div className=" grid grid-cols-3 gap-36">
             <div data-aos="zoom-in" data-aos-duration="3000">
 <div className="flex justify-center"><Image src={logo} className=" w-20" alt=""></Image></div>
-<div className="text-center"> <h1 className="text-2xl font-semibold pb-4">Bachelor's Programmers</h1>
+<div className="text-center"> <h1 className="text-2xl font-semibold pb-4">Bachelor's Programme</h1>
 <p className=" text-lg pb-6">Choose from more than 100,000 Bachelor’s <br /> programmes at universities, <br /> colleges and schools worldwide.</p>
 </div>
 <div className="flex justify-center">
@@ -23,7 +23,7 @@ const LookingCourseSection = () => {
             </div>
             <div data-aos="zoom-in" data-aos-duration="3000">
 <div className="flex justify-center"><Image src={logo} className=" w-20" alt=""></Image></div>
-<div className="text-center"> <h1 className="text-2xl font-semibold pb-4">Master's Programmers</h1>
+<div className="text-center"> <h1 className="text-2xl font-semibold pb-4">Master's Programme</h1>
 <p className=" text-lg pb-6">Find out more than 200,00 Master’s <br /> programmes at universities, <br /> colleges and schools worldwide.</p>
 </div>
 <div className="flex justify-center">
@@ -32,11 +32,11 @@ const LookingCourseSection = () => {
             </div>
             <div data-aos="zoom-in" data-aos-duration="3000">
 <div className="flex justify-center"><Image src={logo} className=" w-20" alt=""></Image></div>
-<div className="text-center"> <h1 className="text-2xl font-semibold pb-4">Phd's Programmers</h1>
+<div className="text-center"> <h1 className="text-2xl font-semibold pb-4">PhD's Programme</h1>
 <p className=" text-lg pb-6">Choose from more than 1000 Phd’s <br /> programmes at universities, <br /> colleges and schools worldwide.</p>
 </div>
 <div className="flex justify-center">
-    <Button>Phd's Programme</Button>
+    <Button>PhD's Programme</Button>
 </div>
             </div>
 
